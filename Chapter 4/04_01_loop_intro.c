89ef0e34162e3291ee330f38f5d9c687903e825f
OM M PATEL <96344622+PATELOM925@users.noreply.github.com>
2024-07-28 00:19:11 +0530

    Update 04_01_loop_intro.c

--- Chapter 4/04_01_loop_intro.c
+++ Chapter 4/04_01_loop_intro.c
@@ -3,7 +3,7 @@
 int main(){
     printf("Hello ");
     printf("world\n");
-    int a = 1;
+    int a = 1; 
     // Loops are used to repeat similar part of a code snippet efficiently
     // (
     // printf("%d\n", a);
